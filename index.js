2cf5d2113a80e568f7fb661fd26ded680b0e1023
carl Ivan Sampan <carlforwebdev@gmail.com>
2024-06-29 08:48:02 +0600

    menu bar implemented

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-
-let obj = [
-    {name: "carl", age: "20"},
-    {name: "carl", age: "20"}
-]
-
-obj.map((item) => {
-    console.log(item.name);
-})  
+document.getElementById('menu-button-id').addEventListener('click', function() {
+    var menu = document.getElementById('menu-container');
+    if (menu.style.display === 'block') {
+        menu.style.display = 'none';
+    } else {
+        menu.style.display = 'block';
+    }
+});
+document.getElementById('menu-button-id').addEventListener('click', function() {
+    var menu = document.getElementById('menu-container-side');
+    if (menu.style.display === 'block') {
+        menu.style.display = 'none';
+    } else {
+        menu.style.display = 'block';
+    }
+});
